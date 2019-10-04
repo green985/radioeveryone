71e98580f8b389e47ff177947dd4b8c63a201285
Erdi Özbek <erdi.ozbek@dreamsandbytes.com>
2019-10-04 11:27:58 +0300

    Project Start
    Base module create.

---
+++ base/src/main/java/com/eiappcompany/base/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.eiappcompany.base
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+/**
+Created by EiAppCompany
+04-10-2019
+11:23
+ **/
+
+abstract class BaseActivity : AppCompatActivity() {
+
+}
